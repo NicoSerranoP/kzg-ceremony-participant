d445d74deeba869b4cc8d3b1cb4d4b135fa3d0e4
Nico Serrano <nnicosp@hotmail.com>
2022-09-12 12:33:33 -0500

    worker(fix): await inside async function

--- wasm/wasm-worker.js
+++ wasm/wasm-worker.js
@@ -1,17 +1,19 @@
 import init, {init_threads, contribute_wasm} from "./pkg/kate_ptau_rs.js";
 
-console.log("available threads:", navigator.hardwareConcurrency);
+(async () => {
+    console.log("available threads:", navigator.hardwareConcurrency);
 
-await init();
-await init_threads(navigator.hardwareConcurrency);
+    await init();
+    await init_threads(navigator.hardwareConcurrency);
 
-fetch('./initialContribution.json').then(response => {
-    response.json().then(data => {
-        var json_string = JSON.stringify(data);
-        var startTime = performance.now()
-        console.log("start");
-        var res = contribute_wasm(json_string);
-        var endTime = performance.now()
-        console.log(`Contribution took ${endTime - startTime} milliseconds`)
+    fetch('./initialContribution.json').then(response => {
+        response.json().then(data => {
+            var json_string = JSON.stringify(data);
+            var startTime = performance.now()
+            console.log("start");
+            var res = contribute_wasm(json_string);
+            var endTime = performance.now()
+            console.log(`Contribution took ${endTime - startTime} milliseconds`)
+        });
     });
-});
+})();
